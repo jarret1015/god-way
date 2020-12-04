be2f0b6e98422b9b07158af7ef628e5b53e9012a
liujiangtao <liujiangtao@jiuaoedu.com>
2020-12-04 11:22:23 +0800

    项目初始化

---
+++ src/test/java/com/jarret/godway/GodWayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jarret.godway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GodWayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
